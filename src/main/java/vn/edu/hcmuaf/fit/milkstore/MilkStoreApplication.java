cd0b4ef9e02156eb1143183c1f6132f8ade72501
Le Huu Tien <22130290@st.hcmuaf.edu.vn>
2026-04-25 15:37:09 +0700

    Initial commit: Setup Spring Boot and React project

---
+++ src/main/java/vn/edu/hcmuaf/fit/milkstore/MilkStoreApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.milkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/hcmuaf/fit/milkstore/MilkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.milkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
